8ba8fbf472c51cb5b43be2124dada0ce581b714b
Tanveer Ahmad <61573422+5Tani@users.noreply.github.com>
2020-12-21 23:16:36 +0500

    test

---
+++ Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frmbloodglucose
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblpatient5 = New System.Windows.Forms.Label()
+        Me.lblpatient4 = New System.Windows.Forms.Label()
+        Me.lblpatient3 = New System.Windows.Forms.Label()
+        Me.lblpatient2 = New System.Windows.Forms.Label()
+        Me.lblpatient1 = New System.Windows.Forms.Label()
+        Me.lblpatient10 = New System.Windows.Forms.Label()
+        Me.lblpatient9 = New System.Windows.Forms.Label()
+        Me.lblpatient8 = New System.Windows.Forms.Label()
+        Me.lblpatient7 = New System.Windows.Forms.Label()
+        Me.lblpatient6 = New System.Windows.Forms.Label()
+        Me.txtpatient1 = New System.Windows.Forms.TextBox()
+        Me.txtpatient6 = New System.Windows.Forms.TextBox()
+        Me.txtpatient7 = New System.Windows.Forms.TextBox()
+        Me.txtpatient8 = New System.Windows.Forms.TextBox()
+        Me.txtpatient9 = New System.Windows.Forms.TextBox()
+        Me.txtpatient10 = New System.Windows.Forms.TextBox()
+        Me.txtpatient2 = New System.Windows.Forms.TextBox()
+        Me.txtpatient3 = New System.Windows.Forms.TextBox()
+        Me.txtpatient4 = New System.Windows.Forms.TextBox()
+        Me.txtpatient5 = New System.Windows.Forms.TextBox()
+        Me.lstop = New System.Windows.Forms.ListBox()
+        Me.btngeneratereport = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblpatient5
+        '
+        Me.lblpatient5.AutoSize = True
+        Me.lblpatient5.Location = New System.Drawing.Point(669, 54)
+        Me.lblpatient5.Name = "lblpatient5"
+        Me.lblpatient5.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient5.TabIndex = 0
+        Me.lblpatient5.Text = "patient 5"
+        '
+        'lblpatient4
+        '
+        Me.lblpatient4.AutoSize = True
+        Me.lblpatient4.Location = New System.Drawing.Point(542, 54)
+        Me.lblpatient4.Name = "lblpatient4"
+        Me.lblpatient4.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient4.TabIndex = 1
+        Me.lblpatient4.Text = "patient 4"
+        '
+        'lblpatient3
+        '
+        Me.lblpatient3.AutoSize = True
+        Me.lblpatient3.Location = New System.Drawing.Point(410, 51)
+        Me.lblpatient3.Name = "lblpatient3"
+        Me.lblpatient3.Size = New System.Drawing.Size(56, 15)
+        Me.lblpatient3.TabIndex = 2
+        Me.lblpatient3.Text = "patient  3"
+        '
+        'lblpatient2
+        '
+        Me.lblpatient2.AutoSize = True
+        Me.lblpatient2.Location = New System.Drawing.Point(286, 54)
+        Me.lblpatient2.Name = "lblpatient2"
+        Me.lblpatient2.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient2.TabIndex = 3
+        Me.lblpatient2.Text = "patient 2"
+        '
+        'lblpatient1
+        '
+        Me.lblpatient1.AutoSize = True
+        Me.lblpatient1.Location = New System.Drawing.Point(148, 54)
+        Me.lblpatient1.Name = "lblpatient1"
+        Me.lblpatient1.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient1.TabIndex = 4
+        Me.lblpatient1.Text = "patient 1"
+        '
+        'lblpatient10
+        '
+        Me.lblpatient10.AutoSize = True
+        Me.lblpatient10.Location = New System.Drawing.Point(669, 130)
+        Me.lblpatient10.Name = "lblpatient10"
+        Me.lblpatient10.Size = New System.Drawing.Size(59, 15)
+        Me.lblpatient10.TabIndex = 5
+        Me.lblpatient10.Text = "patient 10"
+        '
+        'lblpatient9
+        '
+        Me.lblpatient9.AutoSize = True
+        Me.lblpatient9.Location = New System.Drawing.Point(542, 130)
+        Me.lblpatient9.Name = "lblpatient9"
+        Me.lblpatient9.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient9.TabIndex = 6
+        Me.lblpatient9.Text = "patient 9"
+        '
+        'lblpatient8
+        '
+        Me.lblpatient8.AutoSize = True
+        Me.lblpatient8.Location = New System.Drawing.Point(410, 130)
+        Me.lblpatient8.Name = "lblpatient8"
+        Me.lblpatient8.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient8.TabIndex = 7
+        Me.lblpatient8.Text = "patient 8"
+        '
+        'lblpatient7
+        '
+        Me.lblpatient7.AutoSize = True
+        Me.lblpatient7.Location = New System.Drawing.Point(286, 130)
+        Me.lblpatient7.Name = "lblpatient7"
+        Me.lblpatient7.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient7.TabIndex = 8
+        Me.lblpatient7.Text = "patient 7"
+        '
+        'lblpatient6
+        '
+        Me.lblpatient6.AutoSize = True
+        Me.lblpatient6.Location = New System.Drawing.Point(142, 130)
+        Me.lblpatient6.Name = "lblpatient6"
+        Me.lblpatient6.Size = New System.Drawing.Size(53, 15)
+        Me.lblpatient6.TabIndex = 9
+        Me.lblpatient6.Text = "patient 6"
+        '
+        'txtpatient1
+        '
+        Me.txtpatient1.Location = New System.Drawing.Point(124, 72)
+        Me.txtpatient1.Name = "txtpatient1"
+        Me.txtpatient1.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient1.TabIndex = 10
+        '
+        'txtpatient6
+        '
+        Me.txtpatient6.Location = New System.Drawing.Point(124, 148)
+        Me.txtpatient6.Name = "txtpatient6"
+        Me.txtpatient6.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient6.TabIndex = 11
+        '
+        'txtpatient7
+        '
+        Me.txtpatient7.Location = New System.Drawing.Point(261, 148)
+        Me.txtpatient7.Name = "txtpatient7"
+        Me.txtpatient7.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient7.TabIndex = 12
+        '
+        'txtpatient8
+        '
+        Me.txtpatient8.Location = New System.Drawing.Point(385, 148)
+        Me.txtpatient8.Name = "txtpatient8"
+        Me.txtpatient8.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient8.TabIndex = 13
+        '
+        'txtpatient9
+        '
+        Me.txtpatient9.Location = New System.Drawing.Point(513, 148)
+        Me.txtpatient9.Name = "txtpatient9"
+        Me.txtpatient9.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient9.TabIndex = 14
+        '
+        'txtpatient10
+        '
+        Me.txtpatient10.Location = New System.Drawing.Point(640, 148)
+        Me.txtpatient10.Name = "txtpatient10"
+        Me.txtpatient10.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient10.TabIndex = 15
+        '
+        'txtpatient2
+        '
+        Me.txtpatient2.Location = New System.Drawing.Point(261, 69)
+        Me.txtpatient2.Name = "txtpatient2"
+        Me.txtpatient2.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient2.TabIndex = 16
+        '
+        'txtpatient3
+        '
+        Me.txtpatient3.Location = New System.Drawing.Point(385, 69)
+        Me.txtpatient3.Name = "txtpatient3"
+        Me.txtpatient3.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient3.TabIndex = 17
+        '
+        'txtpatient4
+        '
+        Me.txtpatient4.Location = New System.Drawing.Point(513, 69)
+        Me.txtpatient4.Name = "txtpatient4"
+        Me.txtpatient4.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient4.TabIndex = 18
+        '
+        'txtpatient5
+        '
+        Me.txtpatient5.Location = New System.Drawing.Point(640, 69)
+        Me.txtpatient5.Name = "txtpatient5"
+        Me.txtpatient5.Size = New System.Drawing.Size(100, 23)
+        Me.txtpatient5.TabIndex = 19
+        '
+        'lstop
+        '
+        Me.lstop.FormattingEnabled = True
+        Me.lstop.ItemHeight = 15
+        Me.lstop.Location = New System.Drawing.Point(222, 254)
+        Me.lstop.Name = "lstop"
+        Me.lstop.Size = New System.Drawing.Size(357, 184)
+        Me.lstop.TabIndex = 20
+        '
+        'btngeneratereport
+        '
+        Me.btngeneratereport.Location = New System.Drawing.Point(329, 204)
+        Me.btngeneratereport.Name = "btngeneratereport"
+        Me.btngeneratereport.Size = New System.Drawing.Size(156, 23)
+        Me.btngeneratereport.TabIndex = 21
+        Me.btngeneratereport.Text = "Generate Report"
+        Me.btngeneratereport.UseVisualStyleBackColor = True
+        '
+        'Frmbloodglucose
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btngeneratereport)
+        Me.Controls.Add(Me.lstop)
+        Me.Controls.Add(Me.txtpatient5)
+        Me.Controls.Add(Me.txtpatient4)
+        Me.Controls.Add(Me.txtpatient3)
+        Me.Controls.Add(Me.txtpatient2)
+        Me.Controls.Add(Me.txtpatient10)
+        Me.Controls.Add(Me.txtpatient9)
+        Me.Controls.Add(Me.txtpatient8)
+        Me.Controls.Add(Me.txtpatient7)
+        Me.Controls.Add(Me.txtpatient6)
+        Me.Controls.Add(Me.txtpatient1)
+        Me.Controls.Add(Me.lblpatient6)
+        Me.Controls.Add(Me.lblpatient7)
+        Me.Controls.Add(Me.lblpatient8)
+        Me.Controls.Add(Me.lblpatient9)
+        Me.Controls.Add(Me.lblpatient10)
+        Me.Controls.Add(Me.lblpatient1)
+        Me.Controls.Add(Me.lblpatient2)
+        Me.Controls.Add(Me.lblpatient3)
+        Me.Controls.Add(Me.lblpatient4)
+        Me.Controls.Add(Me.lblpatient5)
+        Me.Name = "Frmbloodglucose"
+        Me.Text = "Blood Gulcose"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblpatient5 As Label
+    Friend WithEvents lblpatient4 As Label
+    Friend WithEvents lblpatient3 As Label
+    Friend WithEvents lblpatient2 As Label
+    Friend WithEvents lblpatient1 As Label
+    Friend WithEvents lblpatient10 As Label
+    Friend WithEvents lblpatient9 As Label
+    Friend WithEvents lblpatient8 As Label
+    Friend WithEvents lblpatient7 As Label
+    Friend WithEvents lblpatient6 As Label
+    Friend WithEvents txtpatient1 As TextBox
+    Friend WithEvents txtpatient6 As TextBox
+    Friend WithEvents txtpatient7 As TextBox
+    Friend WithEvents txtpatient8 As TextBox
+    Friend WithEvents txtpatient9 As TextBox
+    Friend WithEvents txtpatient10 As TextBox
+    Friend WithEvents txtpatient2 As TextBox
+    Friend WithEvents txtpatient3 As TextBox
+    Friend WithEvents txtpatient4 As TextBox
+    Friend WithEvents txtpatient5 As TextBox
+    Friend WithEvents lstop As ListBox
+    Friend WithEvents btngeneratereport As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Frmbloodglucose
+    Private Sub btngeneratereport_Click(sender As Object, e As EventArgs) Handles btngeneratereport.Click
+
+        Dim BGlucose(10) As Double
+
+        BGlucose(0) = CDbl(txtpatient1.Text)
+        BGlucose(1) = CDbl(txtpatient2.Text)
+        BGlucose(2) = CDbl(txtpatient3.Text)
+        BGlucose(3) = CDbl(txtpatient4.Text)
+        BGlucose(4) = CDbl(txtpatient5.Text)
+        BGlucose(5) = CDbl(txtpatient6.Text)
+        BGlucose(6) = CDbl(txtpatient7.Text)
+        BGlucose(7) = CDbl(txtpatient8.Text)
+        BGlucose(8) = CDbl(txtpatient9.Text)
+        BGlucose(9) = CDbl(txtpatient10.Text)
+
+        Dim Avg As Double = ComputeAvg(BGlucose)
+        lstop.Items.Add("Average Blood Glucose is: " & Avg)
+        lstop.Items.Add(vbNewLine)
+
+        lstop.Items.Add("Highest Blood Glucose " & FindHighest(BGlucose))
+        lstop.Items.Add("Lowest Blood Glucose" & FindLowest(BGlucose))
+        lstop.Items.Add(vbNewLine)
+
+        Dim Normal As Integar = 0
+        Dim High As Integer = CountNormalHigh(BGlucose, Normal)
+        lstop.Items.Add("Numbers of patient with Normal Blood Glucose: " & Normal)
+
+        lstop.Items.Add("Numbers of Patient with High Blood Glucose " & High)
+        lstop.Items.Add(vbNewLine)
+
+        Call DisplayHighBlood(BGlucose)
+
+
+    End Sub
+
+    Module BloodGlucoseReport
+        Function ComputAvg(ByVal BGlucose() As Double) As Double
+            Dim Sum As Double = 0
+            Dim Avg As Double
+            For i As Integer = 0 To BGlucose.GetUpperBound(0) Step 1
+                Sum += BGlucose(i)
+            Next
+            Avg = Sum / BGlucose.Length
+            Return Avg
+        End Function
+
+        Function FindHighest(ByVal BGlucose() As Double) As Double
+            Dim max As Double = BGlucose(0)
+            For j As Integer = 1 To BGlucose.GetUpperBound(0) Step 1
+                If max < BGlucose(j) Then
+                    max = BGlucose(j)
+                End If
+            Next
+            Return max
+        End Function
+
+
+        Function FindLowest(ByVal BGlucose() As Double) As Double
+            Dim min As Double = BGlucose(0)
+            For j As Integer = 1 To BGlucose.GetUpperBound(0) Step 1
+                If min > BGlucose(j) Then
+                    min = BGlucose(j)
+                End If
+            Next
+            Return min
+            Frmbloodglucose.lstResult.Items.Add(vbNewLine)
+        End Function
+
+        Function CountNormalHigh(ByVal BGlucose() As Double, ByRef Normal As Double) As Double
+            Dim Count As Integer
+
+            For k As Integer = 0 To BGlucose.GetUpperBound(0) Step 1
+                If BGlucose(k) < 6.9 Then
+                    Normal += 1
+                ElseIf BGlucose(k) >= 6.9 Then
+                    Count += 1
+                End If
+            Next
+            Return Count
+        End Function
+
+        Sub DisplayHighBlood(ByVal BGlucose() As Double)
+
+            Frmbloodglucose.lstResult.Items.Add("List of Patient with High Blood Glucose:")
+            Frmbloodglucose.lstResult.Items.Add("Patient" & vbTab & "Blood Glucose")
+            Frmbloodglucose.lstResult.Items.Add("--------------------------------------")
+            For j As Integer = 0 To BGlucose.GetUpperBound(0) Step 1
+                If BGlucose(j) >= 6.9 Then
+                    Frmbloodglucose.lstResult.Items.Add(j + 1 & vbTab & BGlucose(j))
+                End If
+            Next
+        End Sub
+    End Module
+
+End Class
